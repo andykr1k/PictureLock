0432a3a6b101258dc9d78aa45db12437a5cbea4e
andykr1k <69021102+andykr1k@users.noreply.github.com>
2023-06-25 23:37:24 -0700

    Made a under construction page as well as file name change

--- Frontend/Web/PictureLock/src/App.jsx
+++ Frontend/Web/PictureLock/src/App.jsx
@@ -2,8 +2,38 @@ import './App.css'
 
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div class="flex items-center justify-center h-screen bg-gray-200">
+      <div class="container">
+        <div class="bg-white rounded-lg shadow-lg p-10 md:p-20 mx-2">
+          <div class="text-center">
+            <h2 class="text-4xl tracking-tight leading-10 font-extrabold text-gray-900 sm:text-5xl sm:leading-none md:text-6xl">
+              Picture<span class="text-indigo-600">Lock</span>
+            </h2>
+            <h3 class='text-xl md:text-3xl mt-10'>Under Construction...</h3>
+            <p class="text-md md:text-xl mt-10">Unlock the Power of Film with AI: Your Ultimate Social Hub for Movie Enthusiasts.</p>
+          </div>
+          <div class="flex flex-wrap mt-10 justify-center">
+            <div class="m-3">
+              <a target='_blank' href="https://www.facebook.com/picturelock" title="Quicktoolz On Facebook"
+                class="md:w-32 bg-white tracking-wide text-gray-800 font-bold rounded border-2 border-blue-600 hover:border-blue-600 hover:bg-blue-600 hover:text-white shadow-md py-2 px-6 inline-flex items-center">
+                <span class="mx-auto">Instagram</span>
+              </a>
+            </div>
+            <div class="m-3">
+              <a target='_blank' href="https://twitter.com/picturelock" title="Quicktoolz On Twitter"
+                class="md:w-32 bg-white tracking-wide text-gray-800 font-bold rounded border-2 border-blue-500 hover:border-blue-500 hover:bg-blue-500 hover:text-white shadow-md py-2 px-6 inline-flex items-center">
+                <span class="mx-auto">Twitter</span>
+              </a>
+            </div>
+            <div class="m-3">
+              <a target='_blank' href="https://www.github.com/andykr1k/picturelock" title="Quicktoolz On Facebook"
+                class="md:w-32 bg-white tracking-wide text-gray-800 font-bold rounded border-2 border-purple-500 hover:border-purple-500 hover:bg-purple-500 hover:text-white shadow-md py-2 px-6 inline-flex items-center">
+                <span class="mx-auto">Github</span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
