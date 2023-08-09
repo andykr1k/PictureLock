343de252b4ccdf4b142fb9d5a5555dd6dfe6a382
andykr1k <69021102+andykr1k@users.noreply.github.com>
2023-08-08 21:19:30 -0700

    Added V1 Page for Recommender - Scratch

--- Frontend/Mobile/PictureLock/pages/Search.js
+++ Frontend/Mobile/PictureLock/pages/Search.js
@@ -1,17 +1,32 @@
-import { Button, Text, View } from 'react-native';
+import { Button, Text, View, StyleSheet } from 'react-native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React, { useState } from "react";
+import { SearchBar } from '@rneui/themed';
+
 
 function SearchScreen({ navigation }) {
+  const [search, setSearch] = useState("");
+  const updateSearch = (search) => {
+    setSearch(search);
+  };
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Search screen</Text>
-        <Button
-          title="Go to Details"
-          onPress={() => navigation.navigate('Details')}
+      <View style={styles.view}>
+        <SearchBar
+          platform="ios"
+          placeholder="Search"
+          onChangeText={updateSearch}
+          value={search}
+          containerStyle={{ backgroundColor: '#00000000'  }}
         />
       </View>
     );
 }
+
+const styles = StyleSheet.create({
+  view: {
+    margin: 10,
+  },
+  });
 
 function DetailsScreen() {
 return (
