2548b2c6efebcca9b2b0d013da113fe1d6aa50b6
andykr1k <69021102+andykr1k@users.noreply.github.com>
2024-04-30 12:48:38 -0700

    testing vercel package limit

--- Backend/index.py
+++ Backend/index.py
@@ -1,8 +1,8 @@
 from flask import Flask, jsonify
 from flask_cors import CORS, cross_origin
-# import pandas as pd
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
+import pandas as pd
+# from sklearn.feature_extraction.text import CountVectorizer
+# from sklearn.metrics.pairwise import cosine_similarity
 
 # def read():
 #     netflix_movies_df = pd.read_csv('./data/netflix_titles.csv')
@@ -35,93 +35,93 @@ from sklearn.metrics.pairwise import cosine_similarity
 
 #     return movies_df
 
-def train(movies_df):
-    cv = CountVectorizer(max_features=50000,stop_words='english')
+# def train(movies_df):
+#     cv = CountVectorizer(max_features=50000,stop_words='english')
 
-    def combine_features(data):
-        features=[]
-        for i in range(0, data.shape[0]):
-            features.append(data['description'][i] + ' ' + data['genres'][i] + ' ' + str(data['release_year'][i]))
-        return features
+#     def combine_features(data):
+#         features=[]
+#         for i in range(0, data.shape[0]):
+#             features.append(data['description'][i] + ' ' + data['genres'][i] + ' ' + str(data['release_year'][i]))
+#         return features
     
-    movies_df['combined'] = combine_features(movies_df)
+#     movies_df['combined'] = combine_features(movies_df)
 
-    # ps = PorterStemmer()
+#     # ps = PorterStemmer()
 
-    # def stem(text):
-    #     y=[]
-    #     for i in text.split():
-    #         y.append(ps.stem(i))
-    #     return " ".join(y)
+#     # def stem(text):
+#     #     y=[]
+#     #     for i in text.split():
+#     #         y.append(ps.stem(i))
+#     #     return " ".join(y)
 
-    # movies_df['description'] = movies_df['description'].apply(stem)
+#     # movies_df['description'] = movies_df['description'].apply(stem)
 
-    vectors = cv.fit_transform(movies_df['combined']).toarray()
+#     vectors = cv.fit_transform(movies_df['combined']).toarray()
 
-    similarity = cosine_similarity(vectors)
+#     similarity = cosine_similarity(vectors)
 
-    return similarity
+#     return similarity
 
-def recommend_movie(similarity, movies_df, film_type, movie, platforms):
-    movie_index = movies_df[movies_df['title']==movie].index[0]
-    distances = similarity[movie_index]
-    movie_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x:x[1])[1:25]
+# def recommend_movie(similarity, movies_df, film_type, movie, platforms):
+#     movie_index = movies_df[movies_df['title']==movie].index[0]
+#     distances = similarity[movie_index]
+#     movie_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x:x[1])[1:25]
 
-    recommends = []
-    count = 0
+#     recommends = []
+#     count = 0
 
-    for i in movie_list:
-        if count != 5:
-            if movies_df.iloc[i[0]].platform in platforms:
-                if movies_df.iloc[i[0]].type == film_type:
-                    recommends.append(movies_df.iloc[i[0]].title)
-                    count += 1
-        else:
-            break
-    return recommends
+#     for i in movie_list:
+#         if count != 5:
+#             if movies_df.iloc[i[0]].platform in platforms:
+#                 if movies_df.iloc[i[0]].type == film_type:
+#                     recommends.append(movies_df.iloc[i[0]].title)
+#                     count += 1
+#         else:
+#             break
+#     return recommends
 
-# Fix return with 0 in total
-def recommendation_accuracy(recommends, movies_df, type, title, platforms):
-    total = len(recommends)
-    count = 0
-    for x in recommends:
-        if movies_df[movies_df['title']==x].type.values[0] == type:
-            count += 0.5
-        if movies_df[movies_df['title']==x].platform.values[0] in platforms:
-            count += 0.5
-    return "%.0f%%" % (100 * count/total)
+# # Fix return with 0 in total
+# def recommendation_accuracy(recommends, movies_df, type, title, platforms):
+#     total = len(recommends)
+#     count = 0
+#     for x in recommends:
+#         if movies_df[movies_df['title']==x].type.values[0] == type:
+#             count += 0.5
+#         if movies_df[movies_df['title']==x].platform.values[0] in platforms:
+#             count += 0.5
+#     return "%.0f%%" % (100 * count/total)
 
-def log(film_type, movie, platforms):
-    print("Film Type: " + film_type)
-    print("Movie Title: " + movie)
-    for platform in platforms:
-        print("Platform: " + platform)
+# def log(film_type, movie, platforms):
+#     print("Film Type: " + film_type)
+#     print("Movie Title: " + movie)
+#     for platform in platforms:
+#         print("Platform: " + platform)
 
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
-movies_df = read()
-similarity = train(movies_df)
+# movies_df = read()
+# similarity = train(movies_df)
 
 @app.route('/')
 @cross_origin()
 def home():
     return "Welcome to PictureLock's API!"
 
-@app.route('/recommend/<film_type>/<movie>/<platforms>')
-@cross_origin()
-def recommend(film_type, movie, platforms):
-    # movies_df = read()
-    movies_df = ""
-    platforms = platforms.split("-")
-    log(film_type, movie, platforms)
+# @app.route('/recommend/<film_type>/<movie>/<platforms>')
+# @cross_origin()
+# def recommend(film_type, movie, platforms):
+#     # movies_df = read()
+#     movies_df = ""
+#     platforms = platforms.split("-")
+#     log(film_type, movie, platforms)
 
-    recommends = recommend_movie(similarity, movies_df, str(film_type), str(movie), platforms)
-    # acc = recommendation_accuracy(recommends, movies_df, str(film_type), str(movie), platforms)
-    acc = None
-    response_data = {
-        'recommendations': recommends,
-        'accuracy': acc
-    }
+#     recommends = recommend_movie(similarity, movies_df, str(film_type), str(movie), platforms)
+#     # acc = recommendation_accuracy(recommends, movies_df, str(film_type), str(movie), platforms)
+#     acc = None
+#     response_data = {
+#         'recommendations': recommends,
+#         'accuracy': acc
+#     }
 
-    return jsonify(response_data)
+#     return jsonify(response_data)
